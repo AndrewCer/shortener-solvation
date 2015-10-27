311420a4d8945af773db03ebe3313bb1b888b9ea
Andrew Cer <cer.andrew@gmail.com>
2015-10-27 13:50:30 -0600

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("shortenerSolvation", ['ngRoute']);
+
+
+app.config(function ($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController'
+    })
+    $locationProvider.html5Mode(true);
+    // .otherwise({redirectTo: '/'})
+})
