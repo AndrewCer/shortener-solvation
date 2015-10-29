1e7a3a4e51c110b1c4266ea5607c939a333b76f7
Andrew Cer <cer.andrew@gmail.com>
2015-10-28 18:47:17 -0700

    connection of frontend and api complete
    adding validation for http and https

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,18 +1,41 @@
 app.controller('MasterController', ['$scope', function ($scope) {
 }]);
 
-app.controller('HomeController', ['$scope', '$http', function ($scope, $http) {
+app.controller('HomeController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+
   var shortyUrl = function () {
     return 'shor.ty/' + (Math.random()*0xFFFFFF<<0).toString(16);
   }
+
+  var checkUrl = function (longUrl) {
+    var checkArray = longUrl.split('//')
+    if (checkArray[0] === 'http:' || checkArray[0] === 'https:') {
+      console.log(checkArray);
+    }
+    else {
+      $scope.inputError = true;
+    }
+  }
+
   $scope.shortenUrl = function () {
     var hexHolder = shortyUrl();
-    $http.post('api/shortify', {longUrl: $scope.longUrl, shortUrl: hexHolder})
+    // TODO: add client side validation
+    //if validation passes
+    checkUrl($scope.longUrl);
+    // $http.post('api/shortify', {longUrl: $scope.longUrl, shortyUrl: hexHolder})
+    // .then(function (response) {
+    //   if (response.data === true) {
+    //     $scope.postedLongUrl = $scope.longUrl;
+    //     $scope.shortyUrl = hexHolder;
+    //   }
+    // });
+  }
+
+  $scope.redirectUrl = function (clickedUrl) {
+    $http.post('api/redirect', {shortyUrl: clickedUrl})
     .then(function (response) {
-      if (response.data === true) {
-        $scope.postedLongUrl = $scope.longUrl;
-        $scope.shortyUrl = hexHolder;
-      }
+      $window.open('http://www.google.com');
+      // $window.location.href = response.data;
     });
   }
 }]);
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI);
+// var db = require('monk')("localhost/shorty");
+var bookmarks = db.get('bookmarks');
+var users = db.get('users');
+var bcrypt = require('bcrypt');
 
 router.post('/shortify', function (req, res) {
-  console.log(req.body);
-  res.json(true);
+  var longUrl = req.body.longUrl;
+  var shortyUrl = req.body.shortyUrl
+  // TODO: add server side validation
+  //if (validation passes) {
+    // TODO: check db for existing url
+    // bookmarks.findOne({longUrl: longUrl})
+    // .then(function (bookmark) {
+    //   if (bookmark) {
+    //     res.json("bookmark exists")
+    //   }
+    //   else {
+          bookmarks.insert({longUrl: longUrl, shortyUrl: shortyUrl, userName: ""})
+          .then(function () {
+            res.json(true);
+          });
+    //   }
+    // })
+  //}
+  // else {
+  //   res.json(false);
+  // }
+});
+
+router.post('/redirect', function (req, res) {
+  var shortyUrl = req.body.shortyUrl;
+  bookmarks.findOne({shortyUrl: shortyUrl})
+  .then(function (bookmark) {
+    res.json(bookmark.longUrl)
+  });
 });
 
 module.exports = router;
